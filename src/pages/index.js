6e7ed25e942ffeed88aebcfab7a0d7036b5f471b
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2023-05-30 16:10:57 -0600

    added flaticons to the project (#51)
    added to match with the mobile version
    
    Signed-off-by: DiegoSHS <diegoawesome2017@outlook.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,23 +10,23 @@ const HomePage = ({ summary }) => {
     <Form>
       <Header as='h1'>Suelo</Header>
       <Form.Group widths='equal'>
-        <Form.Button circular fluid icon={{ name: 'thermometer half', color: 'red' }} size="massive" content={`Temperatura: ${suelo.tempAvg} ºC`} label='Temperatura del suelo' labelPosition="left" />
-        <Form.Button circular fluid icon={{ name: 'leaf', color: 'green' }} size="massive" content={`Ph: ${suelo.phAvg} Unidades`} label='Nivel de Ph' labelPosition="left" />
-        <Form.Button circular fluid icon={{ name: 'tint', color: 'blue' }} size="massive" content={`Humedad: %${suelo.humAvg}`} label='Humedad del suelo' labelPosition="left" />
+        <Form.Button circular fluid icon={{ name: 'fi fi-rr-thermometer-half',className:'fi-rr-thermometer', color: 'red' }} size="massive" content={`Temperatura: ${suelo.tempAvg} ºC`} label='Temperatura del suelo' labelPosition="left" />
+        <Form.Button circular fluid icon={{ name: 'fi fi-sr-leaf', color: 'green' }} size="massive" content={`Ph: ${suelo.phAvg} Unidades`} label='Nivel de Ph' labelPosition="left" />
+        <Form.Button circular fluid icon={{ name: 'fi fi-sr-humidity', color: 'blue' }} size="massive" content={`Humedad: %${suelo.humAvg}`} label='Humedad del suelo' labelPosition="left" />
       </Form.Group>
       <Divider />
       <Header as='h1'>Ambiente</Header>
       <Form.Group widths='equal'>
-        <Form.Button circular fluid icon={{ name: 'thermometer half', color: 'red' }} size="massive" content={`Temperatura: ${aire.tempAire} ºC`} label='Temperatura del aire' labelPosition="left" />
-        <Form.Button circular fluid icon={{ name: 'lightbulb', color: 'yellow' }} size="massive" content={`Luminosidad: ${aire.lum}`} label='Luminosidad del ambiente' labelPosition="left" />
-        <Form.Button circular fluid icon={{ name: 'tint', color: 'blue' }} size="massive" content={`Humedad: %${aire.humAire}`} label='Humedad del aire' labelPosition="left" />
+        <Form.Button circular fluid icon={{ name: 'fi fi-rr-summer', color: 'red' }} size="massive" content={`Temperatura: ${aire.tempAire} ºC`} label='Temperatura del aire' labelPosition="left" />
+        <Form.Button circular fluid icon={{ name: 'fi fi-sr-bulb', color: 'yellow' }} size="massive" content={`Luminosidad: ${aire.lum}`} label='Luminosidad del ambiente' labelPosition="left" />
+        <Form.Button circular fluid icon={{ name: 'fi fi-sr-humidity', color: 'blue' }} size="massive" content={`Humedad: %${aire.humAire}`} label='Humedad del aire' labelPosition="left" />
       </Form.Group>
       <Divider />
       <Header as='h1'>Otros</Header>
       <Form.Group widths='equal'>
-        <Form.Button circular fluid icon={{ name: 'cloud', color: 'black' }} size="massive" content={`CO2: ${aire.co2} ppm`} label='Cantidad de co2 en el ambiente' labelPosition="left" />
-        <Form.Button circular fluid icon={{ name: 'sun', color: 'yellow' }} size="massive" content={`Rad: ${aire.radiation} W/m²`} label='Radiación solar en el ambiente' labelPosition="left" />
-        <Form.Button circular fluid icon={{ name: 'tint', color: 'brown' }} size="massive" content={`TDS: ${aire.tds} ppm`} label='Total de sólidos en agua' labelPosition="left" />
+        <Form.Button circular fluid icon={{ name: 'fi fi-sr-cloud', color: 'black' }} size="massive" content={`CO2: ${aire.co2} ppm`} label='Cantidad de co2 en el ambiente' labelPosition="left" />
+        <Form.Button circular fluid icon={{ name: 'fi fi-sr-sun', color: 'yellow' }} size="massive" content={`Rad: ${aire.radiation} W/m²`} label='Radiación solar en el ambiente' labelPosition="left" />
+        <Form.Button circular fluid icon={{ name: 'fi fi-sr-humidity', color: 'brown' }} size="massive" content={`TDS: ${aire.tds} ppm`} label='Total de sólidos en agua' labelPosition="left" />
       </Form.Group>
     </Form>
   )
