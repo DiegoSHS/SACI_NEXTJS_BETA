1207eec41817fd80b157f4489d35c14c3e1d67d1
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2023-06-05 11:51:37 -0600

    added new actuators (#56)
    Signed-off-by: DiegoSHS <diegoawesome2017@outlook.com>

---
+++ src/pages/api/saci/sensor/actuators.js
@@ -0,0 +1,19 @@
+import { connex } from "@/models/dbconn"
+import { getActuators } from "@/models/transactions/sensor"
+const handler = async (req, res) => {
+    const { method } = req
+    try {
+        switch (method) {
+            case 'GET':
+                const { collection } = await connex(process.env.SDB, 'sensors')
+                const actuators = await getActuators(collection)
+                return res.status(200).json(actuators)
+            default:
+                return res.status(405).json({ message: `Method ${method} not allowed` })
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+export default handler
