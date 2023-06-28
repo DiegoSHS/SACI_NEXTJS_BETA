0427a4ff3629a0d80d50c01a3c4bcb227a03c3ab
DiegoSHS <diegoawesome2017@outlook.com>
2023-06-28 17:13:08 -0500

    testing oauth
    Signed-off-by: DiegoSHS <diegoawesome2017@outlook.com>

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,18 @@
 import { Layout } from '@/components/Layout'
 import { Context } from '@/context/context'
+import { SessionProvider } from 'next-auth/react'
 import 'semantic-ui-css/semantic.min.css'
 import '@/styles/globals.css'
 import "react-datepicker/dist/react-datepicker.css"
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Context>
-        <Component {...pageProps} />
-      </Context>
-    </Layout>
+    <SessionProvider session={pageProps.session}>
+      <Layout>
+        <Context>
+          <Component {...pageProps} />
+        </Context>
+      </Layout>
+    </SessionProvider>
   )
 }
---
+++ src/pages/login.js
@@ -0,0 +1,30 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Button, Divider, Header, Icon } from "semantic-ui-react";
+
+export default function Login() {
+    const { data } = useSession()
+
+    return (
+        <>
+            <Header size="large">
+                {
+                    data && data.user ? ('Cerrar sesión') : ('Iniciar sesión')
+                }
+            </Header>
+            <Divider />
+            {
+                data && data.user ?
+                    (
+                        <Button basic onClick={()=>signIn()}>
+                            <Icon color="blue" name="fi fi-brands-google" />Google
+                        </Button>
+                    ) :
+                    (
+                        <Button basic onClick={()=>signOut()}>
+                            Cerrar sesión
+                        </Button>
+                    )
+            }
+        </>
+    )
+}
