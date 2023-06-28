8c028d13feae7a45142c16f77af74d89222e8f72
DiegoSHS <diegoawesome2017@outlook.com>
2023-06-28 17:25:46 -0500

    adde form to login view
    Signed-off-by: DiegoSHS <diegoawesome2017@outlook.com>

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,11 +1,11 @@
 import { signIn, signOut, useSession } from "next-auth/react";
-import { Button, Divider, Header, Icon } from "semantic-ui-react";
+import { Button, Divider, Form, Header, Icon } from "semantic-ui-react";
 
 export default function Login() {
     const { data } = useSession()
 
     return (
-        <>
+        <Form>
             <Header size="large">
                 {
                     data && data.user ? ('Cerrar sesión') : ('Iniciar sesión')
@@ -15,17 +15,17 @@ export default function Login() {
             {
                 data && data.user ?
                     (
-                        <Button basic onClick={()=>signOut()}>
+                        <Button basic onClick={() => signOut()}>
                             Cerrar sesión
                         </Button>
-                        
+
                     ) :
                     (
-                        <Button basic onClick={()=>signIn()}>
+                        <Button basic onClick={() => signIn()}>
                             <Icon color="blue" name="fi fi-brands-google" />Google
                         </Button>
                     )
             }
-        </>
+        </Form>
     )
 }
