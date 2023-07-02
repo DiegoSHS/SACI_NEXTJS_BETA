4a5ef648c7766d9269c2bf3df774f52c89d8c5e2
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2023-07-01 19:05:38 -0600

    Loin page updated (#104)
    Signed-off-by: Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,26 +3,25 @@ import { useState } from "react"
 import { Divider, Form, Header } from "semantic-ui-react"
 const HomePage = ({ summary }) => {
   const [view, setView] = useState(false)
-  const activate = () => setView(!view)
   const { aire, suelo } = summary
 
   return (
     <Form>
-      <Header as='h1'>Suelo</Header>
+      <Header size='large'>Suelo</Header>
       <Form.Group widths='equal'>
         <Form.Button basic circular fluid icon={{ name: 'fi fi-rr-thermometer-half', color: 'red' }} size="big" content={`Temperatura: ${suelo.tempAvg} ºC`} label='Temperatura del suelo' labelPosition="left" />
         <Form.Button basic circular fluid icon={{ name: 'fi fi-sr-leaf', color: 'green' }} size="big" content={`Ph: ${suelo.phAvg} Unidades`} label='Nivel de Ph' labelPosition="left" />
         <Form.Button basic circular fluid icon={{ name: 'fi fi-sr-humidity', color: 'blue' }} size="big" content={`Humedad: ${suelo.humAvg}%`} label='Humedad del suelo' labelPosition="left" />
       </Form.Group>
       <Divider />
-      <Header as='h1'>Ambiente</Header>
+      <Header size='large'>Ambiente</Header>
       <Form.Group widths='equal'>
         <Form.Button basic circular fluid icon={{ name: 'fi fi-rr-summer', color: 'red' }} size="big" content={`Temperatura: ${aire.tempAire} ºC`} label='Temperatura del aire' labelPosition="left" />
         <Form.Button basic circular fluid icon={{ name: 'fi fi-sr-bulb', color: 'yellow' }} size="big" content={`Luminosidad: ${aire.lum}`} label='Luminosidad del ambiente' labelPosition="left" />
         <Form.Button basic circular fluid icon={{ name: 'fi fi-sr-humidity', color: 'blue' }} size="big" content={`Humedad: ${aire.humAire}%`} label='Humedad del aire' labelPosition="left" />
       </Form.Group>
       <Divider />
-      <Header as='h1'>Otros</Header>
+      <Header size='large'>Otros</Header>
       <Form.Group widths='equal'>
         <Form.Button basic circular fluid icon={{ name: 'fi fi-sr-cloud', color: 'black' }} size="big" content={`CO2: ${aire.co2} ppm`} label='Cantidad de co2 en el ambiente' labelPosition="left" />
         <Form.Button basic circular fluid icon={{ name: 'fi fi-sr-sun', color: 'yellow' }} size="big" content={`Rad: ${aire.radiation} W/m²`} label='Radiación solar en el ambiente' labelPosition="left" />
@@ -30,33 +29,10 @@ const HomePage = ({ summary }) => {
       </Form.Group>
     </Form>
   )
-  /*
-  return (
-    <Form>
-      <Header size="large">Inicio</Header>
-      <Header as='h1'>Bienvenido</Header>
-      <Header as='h3'>Para iniciar puedes ver un video de preview del uso de este sitio web</Header>
-      <Button style={{ marginBottom: '10px' }} onClick={activate} circular toggle active={view}>Ver</Button>
-      {view ? (
-        <Embed
-          style={{ borderRadius: '10px' }}
-          source="youtube"
-          id="eNI4CsrwMbY"
-          active={true}
-          autoplay={true}
-          defaultActive={true}
-          hd={true}
-          aspectRatio="16:9"
-          brandedUI
-        />
-      ) : (<div></div>)}
-    </Form>
-  )
-  */
 }
 
 export const getServerSideProps = async () => {
-  const { data } = await axios.get(process.env.API_URL+'api/saci/logs/summary')
+  const { data } = await axios.get(process.env.API_URL + 'api/saci/logs/summary')
   return {
     props: {
       summary: data
