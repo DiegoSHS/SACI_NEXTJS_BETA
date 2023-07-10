86d675784a0d1d4e8d94978f1b10021e396928ee
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2023-07-10 16:41:30 -0600

    added water level to summary (#133)
    * faster revalidate (#126)
    
    Signed-off-by: Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
    
    * if name doesnt exist use alt text in toast (#128)
    
    Signed-off-by: DiegoSHS <diegoawesome2017@outlook.com>
    
    * access to property changed (#130)
    
    * if name doesnt exist use alt text in toast
    
    Signed-off-by: DiegoSHS <diegoawesome2017@outlook.com>
    
    * use ?. instead of . in object property
    
    Signed-off-by: DiegoSHS <diegoawesome2017@outlook.com>
    
    ---------
    
    Signed-off-by: DiegoSHS <diegoawesome2017@outlook.com>
    
    * added water level to summary (#132)
    
    * if name doesnt exist use alt text in toast
    
    Signed-off-by: DiegoSHS <diegoawesome2017@outlook.com>
    
    * use ?. instead of . in object property
    
    Signed-off-by: DiegoSHS <diegoawesome2017@outlook.com>
    
    * added water level
    
    Signed-off-by: DiegoSHS <diegoawesome2017@outlook.com>
    
    ---------
    
    Signed-off-by: DiegoSHS <diegoawesome2017@outlook.com>
    
    ---------
    
    Signed-off-by: Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
    Signed-off-by: DiegoSHS <diegoawesome2017@outlook.com>

--- src/pages/api/saci/logs/summary.js
+++ src/pages/api/saci/logs/summary.js
@@ -8,7 +8,6 @@ const summary = async (req, res) => {
         case "GET":
             try {
                 const summary = await getSummary(collection)
-                if (summary.length === 0) return res.status(400).json({ error: "Invalid actuator" })
                 return res.status(200).json(summary)
             } catch (error) {
                 return res.status(500).json({ error: error.message })
